94a63f387d4348a817ccc92345adf6ce6ba9625a
Barbara <zverbarbara@gmai.com>
2023-09-28 23:37:30 +0200

    Naloga 2, done

--- console.php
+++ console.php
@@ -1,2 +1,23 @@
 <?php
+
+//Faza 1: Naloga 1
 echo "Hello world";
+echo "<br />";
+echo "<br />";
+
+//Faza 2: Naloga 2
+$api_url = 'https://api.coinbase.com/v2/prices/BTC-USD/spot';
+$json_data = file_get_contents($api_url);
+$data = json_decode($json_data, true);
+
+if ($data === null || !isset($data['data'])) {
+    echo "Error loading site!";
+    exit();
+}
+
+echo "Currency: " . $data['data']['base'] . "\n";
+echo "<br />";
+echo "Price: " . $data['data']['amount'] . " " . $data['data']['currency'] . "\n";
+
+
+?>
