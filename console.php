c63a062bd5c8cea8af8fbe0e9fd5f45f64564270
Barbara <zverbarbara@gmai.com>
2023-10-15 20:59:44 +0200

    Naloga 3, done

--- console.php
+++ console.php
@@ -1,23 +1,51 @@
 <?php
+// Faza 1: Naloga 1
+echo "Hello world\n";
+echo "\n";
 
-//Faza 1: Naloga 1
-echo "Hello world";
-echo "<br />";
-echo "<br />";
-
-//Faza 2: Naloga 2
-$api_url = 'https://api.coinbase.com/v2/prices/BTC-USD/spot';
-$json_data = file_get_contents($api_url);
+// Faza 2: Naloga 2
+$api_url1 = 'https://api.coinbase.com/v2/prices/BTC-USD/spot';
+$json_data = file_get_contents($api_url1);
 $data = json_decode($json_data, true);
 
 if ($data === null || !isset($data['data'])) {
-    echo "Error loading site!";
+    echo "Error loading site!\n";
     exit();
 }
 
 echo "Currency: " . $data['data']['base'] . "\n";
-echo "<br />";
 echo "Price: " . $data['data']['amount'] . " " . $data['data']['currency'] . "\n";
+echo "\n";
 
+// Faza 3: Naloga 3
+$help_text = "HELP TEXT:
+php console.php <currency_symbol>;
+currency symbols: EUR,EUR, GBP, JPY, CNY, BTC etc;
+EXAMPLE: php console.php ETH\n";
 
+if (!empty($_SERVER['argv'][1]) && strtolower($_SERVER['argv'][1]) === 'help') {
+    echo $help_text;
+    exit();
+}
+
+if (!empty($_SERVER['argv'][1])) {
+    $currency_symbol = strtoupper($_SERVER['argv'][1]);
+
+    $api_url2 = "https://api.coinbase.com/v2/prices/$currency_symbol-USD/spot";
+    $json_data = file_get_contents($api_url2);
+    $data = json_decode($json_data, true);
+
+    if ($data === null || !isset($data['data'])) {
+        echo "Error loading data for $currency_symbol!\n";
+        exit();
+    }
+
+    echo "Price of $currency_symbol in USD: " . $data['data']['amount'] . " USD\n";
+} else {
+    echo $help_text;
+}
 ?>
+
+
+
+
