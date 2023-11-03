033a174eaa5b01a60f06cb75808beb6c378548e5
Your Name <you@example.com>
2023-11-03 18:11:07 +0100

    Naloga 4, done

--- console.php
+++ console.php
@@ -1,9 +1,11 @@
-<?php
-// Faza 1: Naloga 1
+<?php 
+error_reporting(0);
+
+// Faza 1: Naloga 1 
 echo "Hello world\n";
 echo "\n";
 
-// Faza 2: Naloga 2
+// Naloga 2: Izpis podatka o BTC
 $api_url1 = 'https://api.coinbase.com/v2/prices/BTC-USD/spot';
 $json_data = file_get_contents($api_url1);
 $data = json_decode($json_data, true);
@@ -17,12 +19,18 @@ echo sprintf("Currency: %s\n", $data['data']['base']);
 echo sprintf("Price: %s %s\n", $data['data']['amount'], $data['data']['currency']);
 echo "\n";
 
-// Faza 3: Naloga 3
+//Naloga 3: Izpis poljubne valute proti USD
 $help_text = <<<TEXT
 HELP TEXT:
-php console.php <currency_symbol>;
-currency symbols: EUR, GBP, JPY, CNY, BTC, etc.
-EXAMPLE: php console.php ETH
+- To check the price of a single currency:
+  php console.php <currency_symbol>
+  Currency symbols: EUR, GBP, JPY, CNY, BTC, ETH, etc.
+  FOR EXAMPLE: php console.php ETH
+
+- To check the price of any currency pair, provide the symbols of the base and quote currencies:
+  php console.php <base_currency> <quote_currency>
+  Currency symbols: EUR, GBP, JPY, CNY, BTC, ETH, etc.
+  FOR EXAMPLE: php console.php GBP JPY
 TEXT;
 
 if (!empty($_SERVER['argv'][1]) && strtolower($_SERVER['argv'][1]) === 'help') {
@@ -45,7 +53,96 @@ if (!empty($_SERVER['argv'][1])) {
 } else {
     echo $help_text;
 }
-?>
+
+
+// Naloga 3: Izpis za poljubni par:
+// 
+if (count($_SERVER['argv']) == 3) {
+    $base_currency = strtoupper($_SERVER['argv'][1]);
+    $quote_currency = strtoupper($_SERVER['argv'][2]);
+
+    $api_url3 = "https://api.coinbase.com/v2/prices/$base_currency-$quote_currency/spot";
+} else if (count($_SERVER['argv']) == 2) {
+    echo "Provide both the base and quote currency symbols.\n";
+    echo $help_text;
+    exit(1); 
+} else {
+    echo "Invalid arguments. Provide valid currency symbols or use 'help' for usage.\n";
+    exit(1); 
+}
+
+$json_data = file_get_contents($api_url3);
+$data = json_decode($json_data, true);
+
+if ($data === null || !isset($data['data'])) {
+    echo "Error loading data for $base_currency-$quote_currency. Check if the currency pair exists and try again.\n";
+    echo $help_text;
+    exit(1); 
+}
+
+echo "$base_currency-$quote_currency Price:\n";
+echo sprintf("Currency: %s\n", $data['data']['base']);
+echo sprintf("Price: %s %s\n", $data['data']['amount'], $data['data']['currency']);
+
+
+
+// Naloga 4: Odpornost na napačne argumente
+if (count($_SERVER['argv']) == 3) {
+    $base_currency = strtoupper($_SERVER['argv'][1]);
+    $quote_currency = strtoupper($_SERVER['argv'][2]);
+
+    // dolžina valute
+    if (strlen($base_currency) < 3 || strlen($base_currency) > 10 || strlen($quote_currency) < 3 || strlen($quote_currency) > 10) {
+        echo "Invalid currency symbols. Currency symbols should be between 3 and 10 characters.\n";
+        echo $help_text;
+        exit(1); 
+    }
+
+    $api_url4 = "https://api.coinbase.com/v2/prices/$base_currency-$quote_currency/spot";
+} elseif (count($_SERVER['argv']) == 2) {
+    $currency_symbol = strtoupper($_SERVER['argv'][1]);
+
+    if (strlen($currency_symbol) < 3 || strlen($currency_symbol) > 10) {
+        echo "Invalid currency symbol. Currency symbols should be between 3 and 10 characters.\n";
+        echo $help_text;
+        exit(1);
+    }
+
+    $api_url4 = "https://api.coinbase.com/v2/prices/$currency_symbol-USD/spot";
+} else {
+    echo $help_text;
+    exit();
+}
+
+
+if ($json_data === false) {
+    echo "Error loading data for $base_currency-$quote_currency. Please check if the currency pair exists and try again.\n";
+    exit(1); 
+}
+
+$data = json_decode($json_data, true);
+
+if ($data === null || !isset($data['data'])) {
+    echo "Error loading data for $base_currency-$quote_currency. Please check if the currency pair exists and try again.\n";
+    echo $help_text;
+    exit(1); 
+}
+
+echo "$base_currency-$quote_currency Price:\n";
+echo sprintf("Currency: %s\n", $data['data']['base']);
+echo sprintf("Price: %s %s\n", $data['data']['amount'], $data['data']['currency']);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
