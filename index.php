79aab0d2d726a93f6fded4787d4eb89ebcd2d885
Barbara <zverbarbara@gmai.com>
2024-02-06 22:44:14 +0100

    Naloga 6.5, done

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 
 require_once 'vendor/autoload.php';
+require_once 'lib/model.php';
 
 $loader = new Twig\Loader\FilesystemLoader('lib/view/web');
 $twig = new Twig\Environment($loader);
 
-echo $twig->render('about.twig');
+$model = new Model();
+$validCurrencies = $model->getValidCurrencies();
+
+echo $twig->render('list.html.twig', ['currencies' => $validCurrencies['data']]);
--- var_dump.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'lib/model.php';
-
-$model =new Model();
-
-$valid_currecy_symbols = $model->getValidCurrencies();
-var_dump($valid_currecy_symbols);
